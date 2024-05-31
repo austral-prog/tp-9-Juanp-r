dce5decbf8f206a26d8cb16520bfbcd7562f9ba4
Juan Prieto <162722098+Juanp-r@users.noreply.github.com>
2024-05-31 12:13:21 -0300

    dicts.py

--- dicts.py
+++ dicts.py
@@ -1,48 +1,39 @@
-
-def create_inventory(items):
-    """Create a dict that tracks the amount (count) of each element on the `items` list.
-
-    :param items: list - list of items to create an inventory from.
-    :return: dict - the inventory dictionary.
-    """
-    return {}
-
+def  create_inventory(item):
+    inventario = dict()
+    for i in item:
+        if i not in inventario:
+            inventario[i]=item.count(i)
+    return(inventario)
 
 def add_items(inventory, items):
-    """Add or increment items in inventory using elements from the items `list`.
-
-    :param inventory: dict - dictionary of existing inventory.
-    :param items: list - list of items to update the inventory with.
-    :return: dict - the inventory updated with the new items.
-    """
-    return {}
-
+    inventario2 =create_inventory(items)
+    for i in inventory:
+        if i in inventario2:
+            inventory[i]= inventory[i] + inventario2[i]
+            del inventario2[i]
+    for i in inventario2:
+        if i not in inventory:
+            inventory[i]=inventario2[i]
+    return inventory
 
 def decrement_items(inventory, items):
-    """Decrement items in inventory using elements from the `items` list.
-
-    :param inventory: dict - inventory dictionary.
-    :param items: list - list of items to decrement from the inventory.
-    :return: dict - updated inventory with items decremented.
-    """
-    return {}
-
+    inventario2 = create_inventory(items)
+    for i in inventory:
+        if i in inventario2:
+            if inventario2[i] > inventory[i]:
+                inventory[1]=0
+            else:
+                inventory[i]= inventory[i] - inventario2[i]
+    return inventory
 
 def remove_item(inventory, item):
-    """Remove item from inventory if it matches `item` string.
-
-    :param inventory: dict - inventory dictionary.
-    :param item: str - item to remove from the inventory.
-    :return: dict - updated inventory with item removed. Current inventory if item does not match.
-    """
-    return {}
-
+    if item in inventory:
+        del inventory[item]
+    return inventory
 
 def list_inventory(inventory):
-    """Create a list containing all (item_name, item_count) pairs in inventory.
-
-    :param inventory: dict - an inventory dictionary.
-    :return: list of tuples - list of key, value pairs from the inventory dictionary.
-    """
-    return {}
-
+    lista = list()
+    for material in inventory:
+        if inventory[material] > 0:
+            lista.append((material,inventory(material)))
+    return lista
